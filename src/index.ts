7b5a0e2e1afe17ebe55d4da35c24f5e67eecabe2
Derpius <49565664+Derpius@users.noreply.github.com>
2023-01-07 12:45:04 +0000

    Get installation access token

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,33 @@
 import express from "express";
 import * as dotenv from "dotenv";
+import * as fs from "fs";
+import { App } from "octokit";
 
 dotenv.config();
 
-const app = express();
+const server = express();
 const port = process.env.PORT;
+const appId = process.env.APP_ID;
+const privateKey = fs.readFileSync(process.env.PRIVATE_KEY).toString();
 
-app.get("/", (req, res) => {
-	res.send("Hello World!");
+const app = new App({ appId, privateKey });
+
+server.get("/", async (req, res) => {
+	const {
+		data: { id },
+	} = await app.octokit.rest.apps.getOrgInstallation({
+		org: "TAServers",
+	});
+
+	const {
+		data: { token },
+	} = await app.octokit.rest.apps.createInstallationAccessToken({
+		installation_id: id,
+	});
+
+	res.send(token);
 });
 
-app.listen(port, () => {
+server.listen(port, () => {
 	return console.log(`Express is listening at http://localhost:${port}`);
 });
